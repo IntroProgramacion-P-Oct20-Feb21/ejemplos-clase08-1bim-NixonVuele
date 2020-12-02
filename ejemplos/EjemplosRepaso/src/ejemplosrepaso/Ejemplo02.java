4d736b60c37b33d115e2ef81856e7246d8660ff5
Nixon Vuele <njvuele@utpl.edu.ec>
2020-12-02 09:57:37 -0500

    cambio2

--- ejemplos/EjemplosRepaso/src/ejemplosrepaso/Ejemplo02.java
+++ ejemplos/EjemplosRepaso/src/ejemplosrepaso/Ejemplo02.java
@@ -21,7 +21,14 @@ public class Ejemplo02 {
         Scanner entrada = new Scanner(System.in);
         String ciudad;
         String pais;
-        System.out.printf("%s%s", ciudad, pais);
+        System.out.println("Ingrese el nombre de su ciudad");
+        ciudad=entrada.nextLine();
+        System.out.println("Ingrese el nombre de su pais");
+        pais =entrada.nextLine();
+        System.out.printf("Las entradas son: "
+                + "ciudad %s, pais %s\nLa salida del programa debe ser asi:\n"
+                + "%s,situada en el sur del pais"
+                + "\n\n\t%s\n", ciudad, pais,ciudad, pais=pais.toUpperCase());
     }
     
 }
